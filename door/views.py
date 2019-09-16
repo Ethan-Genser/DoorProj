8d12730f19d580c15ab8487352fb15b0ec726bc4
Ethan Genser <ethanp1033@gmail.com>
2019-09-16 01:49:30 -0500

    Adds second motor, modifies motor timings, and adds startserver.sh

--- door/views.py
+++ door/views.py
@@ -1,16 +1,40 @@
+#!/usr/bin/env python3
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 
 from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
 import time
 
 # Opens Door
 def open(self):
     motor_kit = MotorKit()
+
+    # Reeling in the door handle for 2.7 seconds
     start_time = time.time()
     current_time = start_time
-    while (current_time - start_time <= 3):
-        motor_kit.stepper1.onestep()
+    while (current_time - start_time <= 2.5):
+        motor_kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+        motor_kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
         current_time = time.time()
+
+    # Pauses for 5 seconds to give the user time to open the door
+    start_time = time.time()
+    current_time = start_time
+    while (current_time - start_time <= 5):
+        current_time = time.time()
+
+    # Unreels the door handle for 2.5 seconds
+    start_time = time.time()
+    current_time = start_time
+    while (current_time - start_time <= 2.3):
+        motor_kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        motor_kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        current_time = time.time()
+
+    # Releases motor coils
+    motor_kit.stepper1.release()
+    motor_kit.stepper2.release()
---
+++ startserver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo python3 ~/DoorProj/manage.py runserver 0.0.0.0:8080
