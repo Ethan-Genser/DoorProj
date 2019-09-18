448cef306660d5a8f3cbe45ba031caa4bdc8213a
Ethan Genser <ethanp1033@gmail.com>
2019-09-18 16:19:52 -0500

    Enables the server to respond to http requests with an http response object.

--- door/views.py
+++ door/views.py
@@ -2,21 +2,23 @@
 
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 import time
 
 # Opens Door
 def open(self):
+    httpResponse = HttpResponse("Welcome back!")
+    httpResponse['ExitStatus'] = 1
+
     motor_kit = MotorKit()
 
-    # Reeling in the door handle for 2.7 seconds
+    # Reeling in the door handle for 2.4 seconds
     start_time = time.time()
     current_time = start_time
-    while (current_time - start_time <= 2.5):
+    while (current_time - start_time <= 2.4):
         motor_kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
         motor_kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
         current_time = time.time()
@@ -27,10 +29,10 @@ def open(self):
     while (current_time - start_time <= 5):
         current_time = time.time()
 
-    # Unreels the door handle for 2.5 seconds
+    # Unreels the door handle for 2.2 seconds
     start_time = time.time()
     current_time = start_time
-    while (current_time - start_time <= 2.3):
+    while (current_time - start_time <= 2.2):
         motor_kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
         motor_kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
         current_time = time.time()
@@ -38,3 +40,9 @@ def open(self):
     # Releases motor coils
     motor_kit.stepper1.release()
     motor_kit.stepper2.release()
+
+    # Returns HTTP response object
+    httpResponse['ExitStatus'] = 0          # 0-Successful, 1-Unsuccessful
+    httpResponse['DoorStatus'] = 0          # 0-Closed, 1-Operating
+    httpResponse['RoommateStatus'] = 0      # 0-In, 1-Away, 2-DoNotDisturb, 3-Sleeping
+    return httpResponse
